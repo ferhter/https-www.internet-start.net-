6ea5c8d66a58989fddf9d09265c0507422ef7640
ferhter <126424068+ferhter@users.noreply.github.com>
2023-03-04 18:37:28 -0600

    Create telmex.js

---
+++ telmex.js
@@ -0,0 +1,76 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge;chrome=1"/>
+<link href="Cuscss/index.css?2019053005314326-2068654386" rel="stylesheet" type="text/css" />
+<script src="../resource/common/jquery.min.js?2019053005314326-2068654386" type="text/javascript"></script>
+<script src="../resource/common/util.js?2019053005314326-2068654386" type="text/javascript"></script>
+<script language="JavaScript" src="frame.asp"></script>
+<script language="JavaScript" src="../../resource/spanish/bbspdes.html?2019053005314326-2068654386"></script>
+<script language="JavaScript" type="text/javascript">
+var CfgMode ='TELMEX5GV5';
+var countNum = 0;
+
+
+function LoadFrame()
+{
+top.countNum = 0;
+}
+</script>
+<title>HUAWEI</title>
+</head>
+<body onLoad="LoadFrame();"> 
+<div id="main"> 
+  <div id="header"> 
+<script language="JavaScript" type="text/javascript">
+if((CfgMode.toUpperCase() != 'TELMEXACCESS') && (CfgMode.toUpperCase() != 'TELMEXACCESSNV') && (CfgMode.toUpperCase() != 'TELMEXRESALE'))
+{
+var htmlhead = '<div id="headerLogoImg"><div id="ImageTelmex"><span id="ImageTelmexText">';
+htmlhead += '<a href="http://www.telmex.com" target="_blank"><img src="/images/logo_telmex.png" width="125" border="0" /></a></span></div></div>';
+document.write(htmlhead);
+}
+else
+{
+var htmlhead = '<div id="headerLogoImg" style="background: url(../images/banner_logodf.gif) repeat-x center; height: 70px; width: 144px;"></div>';
+document.write(htmlhead);
+}
+</script>
+     
+    <div id="headerContent"> 
+      <div id="headerInfo"> 
+        <div id="headerTitle"></div> 
+        <div id="HerfTelmex"><span id="HerfTelmexText"></span></div>
+<div id="headerLogout"><span id="headerLogoutText"></span> </div> 
+      </div> 
+      <div id="headerTab"> 
+        <ul> </ul> 
+      </div> 
+    </div> 
+    <div id="headerSpace">&nbsp;</div> 
+  </div> 
+  <div id="center"> 
+    <div id="nav"> 
+      <ul> </ul> 
+    </div> 
+    <div id="content"> 
+      <div id="topNav"> <span id="topNavMainMenu"></span>&nbsp;&gt;&nbsp;<span id="topNavSubMenu"></span> </div> 
+      <div id="frameWarpContent"> 
+        <iframe id="frameContent" frameborder="0" height="100%" marginheight="0" marginwidth="0" scrolling="no" width="100%"></iframe> 
+      </div> 
+    </div> 
+  </div> 
+  <div id="footer"> 
+    <table border="0" cellpadding="0" cellspacing="0"> 
+      <tr> 
+        <td id="footerLogo"></td> 
+        <td id="footerText"></td> 
+      </tr> 
+    </table> 
+  </div> 
+  <div id="fresh"> 
+    <iframe frameborder="0" height="100%" marginheight="0" marginwidth="0" scrolling="no" src="refresh.asp" width="100%"></iframe> 
+  </div> 
+</div> 
+</body>
+</html>
