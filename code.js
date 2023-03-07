2ab63344254072227351c28659b1de2129fd03e7
ferhter <126424068+ferhter@users.noreply.github.com>
2023-03-07 12:17:55 -0600

    Create code.js

---
+++ code.js
@@ -0,0 +1,475 @@
+<DOCTYPE html>
+<html>
+<head>
+  <title>Starting page<title>
+  <meta name="verify-admitad" content="3452df1281" >
+  <link rel="search" type="application/opensearchdescription+xml" title="Starting page" href="/opensearch.xml">
+  <link rel="stylesheet" href="/landing.css" type="text/css">
+<head>
+<body>
+<script type="text/javascript">
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter1347623 = new Ya.Metrika({"id":33064023,"webvisor":true,"clickmap":true,"trackLinks":true,"params":{"version":"old"},"userParams":{"version":"old"}});
+        } catch(e) { }
+    });
+
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+<script>
+<noscript><div><img src="//mc.yandex.ru/watch/33064023" style="position:absolute; left:-9999px;" alt=""><div><noscript>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-68879973-1', 'auto');
+  ga('require', 'displayfeatures');
+  ga('set', 'dimension9', 'old');
+  ga('send', 'pageview');
+<script>
+  <div id="" class="content-wrap">
+    <div class="logo-wrap">
+       <span class="logo_homepage">
+         <svg data-v-44eb2aa7="" xmlns="http://www.w3.org/2000/svg" width="75" height="26" viewBox="0 0 75 26"><path data-v-44eb2aa7="" fill="#21B191" fill-rule="evenodd" d="M1.232 9.844H4.76V25H1.232V9.844zM.728 3.58c0-1.296.972-2.268 2.268-2.268 1.296 0 2.268.972 2.268 2.268 0 1.296-.972 2.268-2.268 2.268C1.7 5.848.728 4.876.728 3.58zm22.464 1.008L20.24 6.316a3.814 3.814 0 0 0-1.08-1.512c-.756-.612-1.548-.864-2.592-.864-2.232 0-3.384 1.332-3.384 2.88 0 .72.252 1.98 2.592 2.916l2.412.972c4.428 1.8 5.652 4.32 5.652 7.056 0 4.572-3.24 7.704-7.74 7.704-2.772 0-4.428-1.044-5.652-2.412-1.296-1.44-1.872-3.024-2.016-4.68l3.636-.792c0 1.188.432 2.304 1.008 3.06.684.864 1.692 1.44 3.132 1.44 2.232 0 3.96-1.62 3.96-4.032 0-2.448-1.872-3.456-3.456-4.104l-2.304-.972c-1.98-.828-4.896-2.484-4.896-6.084 0-3.24 2.52-6.336 7.02-6.336 2.592 0 4.068.972 4.824 1.62a8.18 8.18 0 0 1 1.836 2.412zm10.944 8.496h-2.7V25h-3.528V13.084h-1.476v-3.24h1.476V4.3h3.528v5.544h2.7v3.24zM47.6 11.608V9.844h3.528V25H47.6v-1.584c-1.548 1.8-3.456 2.052-4.536 2.052-4.68 0-7.344-3.888-7.344-8.064 0-4.932 3.384-7.956 7.38-7.956 1.116 0 3.06.288 4.5 2.16zm-4.068 1.008c-2.556 0-4.212 2.16-4.212 4.86 0 2.628 1.656 4.824 4.212 4.824 2.232 0 4.284-1.62 4.284-4.788 0-3.312-2.052-4.896-4.284-4.896zM55.592 25V9.844h3.528v1.368c.396-.468.972-.972 1.476-1.26.684-.396 1.368-.504 2.16-.504.864 0 1.8.144 2.772.72l-1.44 3.204c-.792-.504-1.44-.54-1.8-.54-.756 0-1.512.108-2.196.828-.972 1.044-.972 2.484-.972 3.492V25h-3.528zM74.6 13.084h-2.7V25h-3.528V13.084h-1.476v-3.24h1.476V4.3H71.9v5.544h2.7v3.24z"><path><svg>
+       <span>
+    <div>
+    <div class="search-wrap">
+      <script>
+        (function() {
+          var cx = '012684331380167808104:oe5oj--md1a';
+          var gcse = document.createElement('script');
+          gcse.type = 'text/javascript';
+          gcse.async = true;
+          gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+              '//cse.google.com/cse.js?cx=' + cx;
+          var s = document.getElementsByTagName('script')[0];
+          s.parentNode.insertBefore(gcse, s);
+        })();
+      <script>
+      <gcse:searchbox-only
+          autoSearchOnLoad="true"
+          enableHistory="true"
+          queryParameterName="q"
+          resultsUrl="/"
+          enableAutoComplete="true"
+          autoCompleteMatchType="prefix"
+          autoCompleteMaxCompletions="10"
+          autoCompleteMaxPromotions="5"
+          enableImageSearch="true"
+          defaultToImageSearch="false"
+          imageSearchLayout="column"
+          webSearchResultSetSize="20"
+          webSearchSafesearch="moderate"
+          enableOrderBy="false"
+          linkTarget="_blank"
+          safeSearch="moderate"
+          adclient="partner-pub-9610703105450905"
+          adchannel="4498776854"
+      >
+      <gcse:searchbox-only>
+    <div>
+    <div class="partners-link_wrapper">
+      <ul>
+        <li class="partners-link_tile">
+          <a onclick="window.location.href='https://internet-start.net/?q=youtube#gsc.tab=0&gsc.q=youtube&gsc.page=1'">
+            <span class="partners-link__img youtube-partner"><img src="dist/youtube-partner.svg"><span>
+            <a class="partners-link_name">YouTube<a>
+          <a>
+        <li>
+        <li class="partners-link_tile">
+          <a onclick="window.location.href='http://internet-start.net/?q=facebook#gsc.tab=0&gsc.q=facebook&gsc.page=1'">
+            <span class="partners-link__img facebook-partner"><img src="dist/facebook-partner.svg"><span>
+            <a class="partners-link_name">Facebook<a>
+          <a>
+        <li>
+        <li class="partners-link_tile">
+          <a onclick="window.location.href='https://internet-start.net/?q=Twitter#gsc.tab=0&gsc.q=Twitter&gsc.page=1'">
+            <span class="partners-link__img"><img src="dist/twitter-partner.svg"><span>
+            <a class="partners-link_name">Twitter<a>
+          <a>
+        <li>
+        <li class="partners-link_tile">
+          <a onclick="window.location.href='https://internet-start.net/?q=zoom#gsc.tab=0&gsc.q=zoom&gsc.page=1'">
+            <span class="partners-link__img"><img src="dist/zoom-partner.svg"><span>
+            <a class="partners-link_name">Zoom<a>
+          <a>
+        <li>
+        <li class="partners-link_tile">
+          <a onclick="window.location.href='https://internet-start.net/?q=netflix#gsc.tab=0&gsc.q=netflix&gsc.page=1'">
+            <span class="partners-link__img"><img src="dist/netflix-partner.svg"><span>
+            <a class="partners-link_name">Netflix<a>
+          <a>
+        <li>
+        <li class="partners-link_tile">
+          <a onclick="window.location.href='https://internet-start.net/?q=aliexpress#gsc.tab=0&gsc.q=aliexpress&gsc.page=1'">
+            <span class="partners-link__img"><img src="dist/ali-partner.svg"><span>
+            <a class="partners-link_name">AliExpress<a>
+          <a>
+        <li>
+        <li class="partners-link_tile">
+          <a onclick="window.location.href='https://internet-start.net/?q=amazon#gsc.tab=0&gsc.q=amazon&gsc.page=1'">
+            <span class="partners-link__img"><img src="dist/amazon-partner.svg"><span>
+            <a class="partners-link_name">Amazon<a>
+          <a>
+        <li>
+        <li class="partners-link_tile">
+          <a onclick="window.location.href='https://internet-start.net/?q=apple#gsc.tab=0&gsc.q=apple&gsc.page=1'">
+            <span class="partners-link__img"><img src="dist/apple-partner.svg"><span>
+            <a class="partners-link_name">Apple<a>
+          <a>
+        <li>
+        <li class="partners-link_tile">
+          <a onclick="window.location.href='https://internet-start.net/?q=ebay#gsc.tab=0&gsc.q=ebay&gsc.page=1'">
+            <span class="partners-link__img"><img src="dist/ebay-partner.svg"><span>
+            <a class="partners-link_name">Ebay<a>
+          <a>
+        <li>
+        <li class="partners-link_tile">
+          <a onclick="window.location.href='https://internet-start.net/?q=ikea#gsc.tab=0&gsc.q=ikea&gsc.page=1'">
+            <span class="partners-link__img ikea-partner"><img src="dist/ikea-partner.svg"><span>
+            <a class="partners-link_name">Ikea<a>
+       @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+
+html,
+body {
+  height: 100%;
+  min-height: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+html {
+  overflow: hidden;
+  overflow-y: auto;
+  background-color: #f7f7f7;
+  font-size: 90%;
+  -webkit-font-smoothing: subpixel-antialiased;
+  -webkit-text-size-adjust: 100%;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  -ms-box-sizing: border-box;
+  -o-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+body {
+  overflow: scroll;
+  position: relative;
+  background-color: #fff;
+  line-height: 1.6;
+  color: #333;
+}
+
+.site-wrapper-border {
+  display: block;
+  position: absolute;
+  height: 4px;
+  background: #de5833;
+  z-index: 20;
+  width: 100%;
+  left: 0px;
+  top: 0px;
+}
+
+.logo_homepage svg{
+  width: 100%;
+  height: auto;
+}
+
+@media only screen and (max-width: 634px) {
+  html {
+    min-width: initial;
+  }
+}
+
+.header-wrap {
+  border-top: 4px solid #de5833;
+  padding-top: 16px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  z-index: 5;
+}
+
+.content-wrap {
+  position: absolute;
+  margin: auto;
+  width: 100%;
+  top: 13%;
+  left: 0;
+  right: 0;
+  z-index: 2;
+}
+
+.logo-wrap {
+  position: relative;
+  display: block;
+  max-width: 180px;
+  margin: auto;
+  margin-bottom: 5px;;
+  margin-top: -1.25em;
+}
+
+@media only screen and (max-width: 425px) {
+  .logo-wrap {
+    margin-bottom: 1.25em;
+    margin-top: 0;
+  }
+}
+
+@media only screen and (max-height: 382.5px) and (max-width: 634px), only screen and (max-height: 361.25px) {
+  .logo-wrap {
+    margin-bottom: 0;
+    margin-top: 0;
+  }
+}
+
+.search-wrap {
+  padding: 1.3em 0.8em 1em;
+  max-width: 634px;
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+  float: none;
+  width: 70%;
+}
+
+@media only screen and (max-width: 634px) {
+  .search-wrap {
+    width: 80%;
+  }
+}
+
+@media only screen and (max-width: 425px) {
+  .search-wrap {
+    width: auto;
+  }
+}
+
+@media only screen and (max-height: 361.25px) and (min-width: 864px) {
+  .content-wrap {
+    margin-bottom: 3em;
+    text-align: center;
+  }
+
+  .logo-wrap,
+  .search-wrap {
+    display: table-cell;
+    vertical-align: middle;
+  }
+
+  .search-wrap {
+    text-align: left;
+  }
+
+  .logo_homepage {
+    margin-left: -22px;
+    margin-right: -22px;
+  }
+}
+
+@media only screen and (max-height: 634px) {
+  .content-wrap {
+    top: 3%;
+  }
+}
+
+@media only screen and (max-height: 595px) and (min-width: 634px) {
+  .content-wrap {
+    position: relative;
+    top: 0;
+    padding-top: 7em;
+    padding-bottom: 4em;
+    margin-bottom: 0;
+  }
+}
+
+@media only screen and (max-height: 318.75px) and (min-width: 634px) {
+  .content-wrap {
+    padding-top: 4em;
+  }
+}
+
+@media only screen and (max-height: 276.25px) and (min-width: 634px) {
+  .content-wrap {
+    position: absolute;
+    padding-top: 0;
+    top: 30%;
+  }
+}
+table.gsc-search-box td.gsc-input,
+table.gsc-search-box td.gsc-search-button {
+  vertical-align: top;
+}
+
+table.gsc-search-box td.gsc-input {
+  padding-right: 0 !important;
+}
+
+input.gsc-search-button.gsc-search-button-v2 {
+  padding: 9px 27px;
+  margin-top: 0;
+}
+.gscb_a {
+  color: #80868b !important;
+  margin-top: 2px;
+}
+.gstl_50 .gsib_a {
+  padding: 10px;
+}
+.gsc-input .gsc-input-box {
+  height: 42px;
+  border: 1px solid #efefef !important;
+  border-radius: 4px;
+  box-shadow: 0 1px 13px rgba(230, 230, 230, 0.3);
+}
+
+.gssb_e {
+  border-radius: 4px;
+}
+
+.gsc-completion-container {
+  border: 1px solid #efefef !important;
+  box-shadow: none !important;
+  border-radius: 4px;
+}
+
+.gsc-search-button {
+  display: none;
+}
+
+/* */
+.lfbar {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  padding-top: 12px;
+  padding-bottom: 5px;
+  margin-top: 20px;
+  line-height: 40px;
+  background: #f2f2f2;
+  border-top: 1px solid #e4e4e4;
+  text-align: center;
+  z-index: 2;
+}
+.lfbar_inner {
+  max-width: 700px;
+}
+.lfbar_link,
+.lfbar_link:hover,
+.lfbar_link:active {
+  color: #777;
+}
+.lfbar_link {
+  padding: 0 13px;
+  text-decoration: none;
+  font-family: sans-serif;
+}
+
+/* Partners block */
+
+.partners-link_wrapper {
+  display: block;
+  width: 88%;
+  max-width: 1280px;
+  margin: 22px auto 80px auto;
+  text-align: center;
+}
+
+.partners-link_wrapper ul {
+  padding-left: 20px;
+  list-style-type: none;
+  text-align: left;
+}
+
+.partners-link_tile {
+  display: inline-block;
+  width: 186px;
+  margin: 20px 20px 0 0;
+  text-align: center;
+  cursor: pointer;
+  transition: 200ms all;
+}
+
+.partners-link_tile:hover .partners-link__img {
+  box-shadow: 0 8px 11px rgba(230, 230, 230, 0.4);
+}
+
+.partners-link_tile:hover {
+  transform: scale(1.05);
+}
+
+.partners-link_tile a:focus {
+  outline: none;
+  opacity: 0.7;
+}
+
+.lenovo-partner {
+  background-color: #e21e14;
+}
+
+.lenovo-partner img {
+  border-radius: 10px;
+}
+
+.partners-link__img {
+  display: flex;
+  width: 175px;
+  height: 90px;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 7px;
+  text-align: center;
+  border: 1px solid #efefef;
+  border-radius: 10px;
+  box-shadow: 0 8px 13px rgba(230, 230, 230, 0.3);
+}
+
+.ikea-partner {
+  background-color: #0057a5;
+}
+
+.partners-link__img img {
+  display: inline;
+  max-width: 145px;
+  padding: 15px;
+}
+
+.partners-link_name {
+  text-decoration: none;
+  font-size: 16px;
+  font-family: 'Roboto', sans-serif;
+}
+
+ Partners block
+   <a>
+        <li>
+      <ul>
+    <div>
+  <div>
+<div>
+<div class="lfbar">
+    <span class="lfbar_inner">
+        <a class="lfbar_link" target="_blank" href="//support.google.com/websearch/?p=ws_results_help&amp;hl=en-RU&amp;fg=1">Help<a>
+        <a class="lfbar_link" target="_blank" href="//www.google.ru/intl/en/policies/privacy/?fg=1">Privacy<a>
+        <a class="lfbar_link" target="_blank" href="//www.google.ru/intl/en/policies/terms/?fg=1">Terms<a>
+    <span>
+<div>
+
+<body>
+<html>
