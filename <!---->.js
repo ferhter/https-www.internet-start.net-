13ab3c47b3f9abc906ad53908aa37e8bbc18ee89
ferhter <126424068+ferhter@users.noreply.github.com>
2023-03-29 13:34:55 -0600

    Create <!---->.js

---
+++ <!---->.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Starting page<title>
+  <meta name="verify-admitad" content="3452df1281" >
+  <link rel="search" type="application/opensearchdescription+xml" title="Starting page" href="/opensearch.xml">
+  <link rel="stylesheet" href="/landing.css" type="text/css">
+<head>
+<body>
+<script type="text/javascript">
+<!---->
