a9e18c4a44ee130ec9a21f9e9a164ae0117ddd67
ferhter <126424068+ferhter@users.noreply.github.com>
2023-04-22 16:51:15 -0600

    Update <!---->.js

--- <!---->.js
+++ <!---->.js
@@ -1,23 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Starting page<title>
-  <meta name="verify-admitad" content="3452df1281" >
-  <link rel="search" type="application/opensearchdescription+xml" title="Starting page" href="/opensearch.xml">
-  <link rel="stylesheet" href="/landing.css" type="text/css">
-<head>hex: 12e02b0ef972c51ed9c833d56e3fc38746f8162eba08a8fef70f2dca909ece27
+hex: 12e02b0ef972c51ed9c833d56e3fc38746f8162eba08a8fef70f2dca909ece27
 HEX: 12E02B0EF972C51ED9C833D56E3FC38746F8162EBA08A8FEF70F2DCA909ECE27
 h:e:x: 12:e0:2b:0e:f9:72:c5:1e:d9:c8:33:d5:6e:3f:c3:87:46:f8:16:2e:ba:08:a8:fe:f7:0f:2d:ca:90:9e:ce:27
 base64: EuArDvlyxR7ZyDPVbj/Dh0b4Fi66CKj+9w8typCezic=
-<body>
-<script type="text/javascript">
-<!---->
+
+<!-🐛--->
 hex: 84cffddbad85a7aa22ff611281a6175720a08c61c23029a59d5e8de07d516e82
 HEX: 84CFFDDBAD85A7AA22FF611281A6175720A08C61C23029A59D5E8DE07D516E82
 h:e:x: 84:cf:fd:db:ad:85:a7:aa:22:ff:61:12:81:a6:17:57:20:a0:8c:61:c2:30:29:a5:9d:5e:8d:e0:7d:51:6e:82
 base64: hM/9262Fp6oi/2ESgaYXVyCgjGHCMCmlnV6N4H1RboI=
-  <---->
+  
+<--🐛-->
 hex: d23456aa73fbc2585340095f73ea2350088ecd4dbcc22c5ce26e5eaec920bc0b
 HEX: D23456AA73FBC2585340095F73EA2350088ECD4DBCC22C5CE26E5EAEC920BC0B
 h:e:x: d2:34:56:aa:73:fb:c2:58:53:40:09:5f:73:ea:23:50:08:8e:cd:4d:bc:c2:2c:5c:e2:6e:5e:ae:c9:20:bc:0b
 base64: 0jRWqnP7wlhTQAlfc+ojUAiOzU28wixc4m5erskgvAs=
+<--🐛--🐛---🐛----🐛---🐛-🐛-🐛-🐛-🐛---🐛----🐛---🐛--🐛-->
